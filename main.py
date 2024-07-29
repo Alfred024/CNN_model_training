c13a1f6d6df8d5f395b130db2ac7e83eef41c3fd
Alfredo <21030761@itcelaya.edu.mx>
2024-07-28 18:24:49 -0600

    Function to predict using model trained

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 # LIBRARIES IMPORTATION
 
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras import models, layers
 import matplotlib.pyplot as plt
 
 # DATA LOADING
 
-IMAGE_SIZE = 256
+IMAGE_SIZE = (256, 256)
 BATCH_SIZE = 32
 EPOCHS = 50
 CHANNELS = 3
 
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
     "Imgs_Folder",
+    seed=123,
     shuffle = True,
     image_size= IMAGE_SIZE,
     batch_size= BATCH_SIZE # batches are like a group of various images
 )
 
 # Result = 65 batches
+class_names = dataset.class_names
 
 # SPLIT OF DATA
 
-def splitDataset(dataset, train_bacthSize, testing_batchSize):
-    # 80% training = 52 batches
-  train_dataset = dataset.take(train_bacthSize)
-    # 10% testing = 7 batches
-  testing_dataset = dataset.skip(train_bacthSize).take(testing_batchSize)
-    # 10% validation = 6 batches
-  validation_dataset = dataset.skip(train_bacthSize).skip(testing_batchSize)
+# SPLIT OF DATA
 
-  return train_dataset, testing_dataset, validation_dataset
-  
-train_dataset, testing_dataset, validation_dataset = splitDataset(dataset, 52, 7)
+def get_splitDataset(dataset, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    
+    ds_size = len(dataset)
+    
+    if shuffle:
+        dataset = dataset.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_dataset = dataset.take(train_size)    
+    validation_dataset = dataset.skip(train_size).take(val_size)
+    testing_dataset = dataset.skip(train_size).skip(val_size)
+    
+    return train_dataset, validation_dataset, testing_dataset
+
+train_dataset, validation_dataset, testing_dataset = get_splitDataset(dataset)
 
 # PREFETCHING + CACHING IMAGE 
 # (In this case, cache would help storing 
@@ -46,7 +58,7 @@ validation_dataset = validation_dataset.cache().shuffle(1000).prefetch(buffer_si
 # To make our model more robust 
 
 resize_and_rescale = tf.keras.Sequential([
-    layers.experimental.preprocessing.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+    layers.experimental.preprocessing.Resizing(IMAGE_SIZE[0], IMAGE_SIZE[1]),
     layers.experimental.preprocessing.Rescaling(1.0/255)
 ])
 
@@ -55,9 +67,9 @@ data_augmentation = tf.keras.Sequential([
     layers.experimental.preprocessing.RandomRotation(0.2)
 ])
 
-# MODEL CREATION 
+# MODEL TRAINING
 
-input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+input_shape = IMAGE_SIZE + (CHANNELS,)
 n_classes = 3
 
 model = models.Sequential([
@@ -79,4 +91,44 @@ model = models.Sequential([
     layers.Dense(n_classes, activation='softmax'),
 ])
 
-model.build(input_shape=input_shape)
+# El build se realiza automáticamente
+# model.build(input_shape=input_shape)
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_dataset,
+    # batch_size=BATCH_SIZE,
+    validation_data=validation_dataset,
+    verbose=1,
+    epochs=EPOCHS,
+)
+
+# FUNCTION TO PREDICT BASED ON A IMAGE
+
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+plt.figure(figsize=(15, 15))
+for images, labels in testing_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        
+        predicted_class, confidence = predict(model, images[i].numpy())
+        actual_class = class_names[labels[i]] 
+        
+        plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
+        
+        plt.axis("off")
